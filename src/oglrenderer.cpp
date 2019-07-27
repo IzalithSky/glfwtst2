61e2822c25d73b7d080b019f31337e47e6da42a7
Sky <sky@mail.com>
2019-07-27 17:37:21 +0100

    assimp 4 lib added, multiple "model" instances rendered
    	modified:   CMakeLists.txt
    	modified:   src/oglrenderer.cpp

--- src/oglrenderer.cpp
+++ src/oglrenderer.cpp
@@ -18,8 +18,8 @@ public:
     void run();
 };
 
-unsigned int SCR_WIDTH = 379;
-unsigned int SCR_HEIGHT = 480;
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
 
 // camera
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
@@ -31,7 +31,7 @@ float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 
 float pitch =  0.0f;
 float lastX =  800.0f / 2.0;
 float lastY =  600.0 / 2.0;
-float fov   =  90.0f;
+float fov   =  70.0f;
 
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
@@ -47,7 +47,6 @@ unsigned int texture;
 
 void error_callback(int error, const char* description);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 bool initGLFW(GLFWwindow* &window);
@@ -58,38 +57,87 @@ struct
     float r, g, b;
     float tx, ty;
 } vertices[] =
-    {
-        { -0.9f, -0.9f,   0.f,    1.f, 0.f, 0.f,    1.0f,  1.0f},
-        { -0.9f,  0.9f,   0.f,    0.f, 1.f, 0.f,    1.0f,  0.0f},
-        {  0.9f, -0.9f,   0.f,    0.f, 1.f, 0.f,    0.0f,  1.0f},
-        {  0.9f,  0.9f,   0.f,    0.f, 0.f, 1.f,    0.0f,  0.0f}
-    };
+{
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    {  0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    {  0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    {  0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    { -0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
 
-unsigned int indices[] =
-    {
-        0, 1, 2,
-        1, 2, 3
-    };
+    { -0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    {  0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    { -0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    { -0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+
+    { -0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    { -0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    { -0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    { -0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    {  0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    {  0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    {  0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    {  0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    {  0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    {  0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    {  0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    { -0.5f, -0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    { -0.5f, -0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+
+    { -0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f },
+    {  0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   1.0f, 1.0f },
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    {  0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   1.0f, 0.0f },
+    { -0.5f,  0.5f,  0.5f,   0.f, 0.f, 0.f,   0.0f, 0.0f },
+    { -0.5f,  0.5f, -0.5f,   0.f, 0.f, 0.f,   0.0f, 1.0f }
+};
+
+unsigned int indices[] = {
+    0, 1, 2,
+    1, 2, 3
+};
+    
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = 2.5 * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;   
+}
 
 void error_callback(int error, const char* description)
 {
     std::cout << stderr << "Error: " << description << std::endl;
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    
-    float cameraSpeed = 25.5 * deltaTime;
-    if (key == GLFW_KEY_W && action == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
-    if (key == GLFW_KEY_S && action == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
-    if (key == GLFW_KEY_A && action == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    if (key == GLFW_KEY_D && action == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -154,7 +202,7 @@ bool initGLFW(GLFWwindow* &window)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    window = glfwCreateWindow(379, 480, "OGL tst", NULL, NULL);
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OGL tst", NULL, NULL);
     if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -162,7 +210,6 @@ bool initGLFW(GLFWwindow* &window)
         return false;
     }
     
-    glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
     
@@ -192,6 +239,8 @@ void oglRenderer() {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    
+    glEnable(GL_DEPTH_TEST);
     
     // === vao, vbo, ebo ==================================
     glGenVertexArrays(1, &vertex_array);
@@ -258,7 +307,7 @@ void oglRendererDestroy() {
     glDeleteBuffers(1, &element_buffer);
     glDeleteBuffers(1, &vertex_buffer);
     glfwDestroyWindow(window);
-    glfwTerminate();    
+    glfwTerminate();
 }
 
 void oglRun() {
@@ -269,11 +318,13 @@ void oglRun() {
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
+        
+        processInput(window);
 
         // === clear ==========================================        
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         glBindTexture(GL_TEXTURE_2D, texture);
         
@@ -283,16 +334,25 @@ void oglRun() {
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         model = glm::translate(model, glm::vec3(0.0f,  0.0f,  0.0f));
-        float angle = 20.0f * 0;
-        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
         
         glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, &projection[0][0]);
         glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, &view[0][0]);
-        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, &model[0][0]);
+
         
         // === transform ======================================
         
-        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+        // glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+        // glDrawArrays(GL_TRIANGLES, 0, 36); // sizeof(indices));
+        
+        for (unsigned int i = 0; i < 10; i++) {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+            glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, &model[0][0]);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         
         glfwSwapBuffers(window);
         glfwPollEvents();
